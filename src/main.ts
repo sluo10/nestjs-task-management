3dac3f142354e41a46ae5457aeb9b5e1dabc1d72
Samuel Luo <samuel@otus.com>
2020-04-25 17:02:16 -0500

    Added Swagger implementation

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
@@ -6,6 +7,14 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   const port = 3000;
+  const options = new DocumentBuilder()
+    .setTitle('Task Management App')
+    .setDescription('Task Management API Description')
+    .setVersion('1.0')
+    .addTag('task')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
